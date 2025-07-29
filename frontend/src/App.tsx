2bef6c6ca6b9f301e69370abfc23b33dc56cca4e
terabayashik <terabayashik@mediaedge.co.jp>
2025-07-29 21:54:26 +0900

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+function App() {
+  return <Box>Hello</Box>;
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,13 @@
+import { MantineProvider } from "@mantine/core";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>,
+);
